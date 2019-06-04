3a16152be65993a42224b74792c58c2010d169ca
Arjun Modi <mrarjunmodi@gmail.com>
2019-06-04 17:22:35 +0100

    fixed images for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
       inject: 'body'
     }),
     new CopyWebpackPlugin([
-      { from: 'src/assets', to: 'assets' }
+      { from: 'src/images', to: 'images' }
     ])
   ]
 }
